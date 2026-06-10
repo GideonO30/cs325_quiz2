d0df3b71f8b5ba90a905d504004cbcb1aaea3c2d
GideonO30 <gigi_okai3696@yahoo.com>
2026-06-10 14:08:22 -0500

    Fix main branch calculator

--- calculator.py
+++ calculator.py
@@ -4,13 +4,5 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
-
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
-print("Multiplication:", multiply(10, 5))
-print("Division:", divide(10, 5))
